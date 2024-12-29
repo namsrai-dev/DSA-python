1ae502d9419aa18d6ceaf7171ef061aa13d93b74
Namsraiav-Davaa <namsraijav0415@gmail.com>
2024-12-30 00:01:01 +0800

    check

--- doublyLinkedList/pop_first.py
+++ doublyLinkedList/pop_first.py
@@ -20,3 +20,9 @@ class DoublyLinkedList:
     def append(self, value):
         new_node = Node(value)
         if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
