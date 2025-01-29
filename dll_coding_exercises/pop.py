38e944cb289e35d4c32ce550161fd93c4d659d61
Namsraiav-Davaa <namsraijav0415@gmail.com>
2025-01-29 09:25:02 +0800

    dll pop

--- dll_coding_exercises/pop.py
+++ dll_coding_exercises/pop.py
@@ -27,16 +27,31 @@ class DoublyLinkedList:
             self.tail.next = new_node
             new_node.prev = self.tail
             self.tail = new_node
-
         self.length += 1
+        return True
+
+    def pop(self):
+        temp = self.tail
+        if self.length == 0:
+            return None
+        elif self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+
+        self.length -= 1
+        return temp
 
 
-    ## WRITE APPEND METHOD HERE ##
-    #                            #
-    #                            #
-    #                            #
-    #                            #
-    ##############################
+    ### WRITE POP METHOD HERE ###
+    #                           #
+    #                           #
+    #                           #
+    #                           #
+    #############################
 
 
 
@@ -45,24 +60,20 @@ my_doubly_linked_list = DoublyLinkedList(1)
 my_doubly_linked_list.append(2)
 
 
-print('Head:', my_doubly_linked_list.head.value)
-print('Tail:', my_doubly_linked_list.tail.value)
-print('Length:', my_doubly_linked_list.length, '\n')
-
-print('Doubly Linked List:')
-my_doubly_linked_list.print_list()
+# (2) Items - Returns 2 Node
+print(my_doubly_linked_list.pop().value)
+# (1) Item -  Returns 1 Node
+print(my_doubly_linked_list.pop().value)
+# (0) Items - Returns None
+print(my_doubly_linked_list.pop())
 
 
 
 """
     EXPECTED OUTPUT:
     ----------------
-    Head: 1
-    Tail: 2
-    Length: 2
-
-    Doubly Linked List:
-    1
     2
+    1
+    None
 
 """
