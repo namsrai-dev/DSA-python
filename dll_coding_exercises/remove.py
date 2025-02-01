da1e074331d0d9c5e7d43e30c956bdae20c1bf03
Namsraiav-Davaa <namsraijav0415@gmail.com>
2025-02-01 21:22:30 +0800

    push

--- dll_coding_exercises/remove.py
+++ dll_coding_exercises/remove.py
@@ -91,31 +91,44 @@ class DoublyLinkedList:
         return False
 
     def insert(self, index, value):
-        new_node = Node(value)
         if index < 0 or index > self.length:
             return False
-        elif index == 0:
-            self.prepend(value)
-        elif index == self.length:
-            self.append(value)
-        else:
-            after = self.get(index)
-            before = after.prev
-            before.next = new_node
-            new_node.next = after
-            after.prev = new_node
-            self.length += 1
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+
+        new_node = Node(value)
+        before = self.get(index - 1)
+        after = before.next
+
+        new_node.prev = before
+        new_node.next = after
+        before.next = new_node
+        after.prev = new_node
+
+        self.length += 1
         return True
 
+    def remove(self, index):
+        current = None
+        if index < 0 or index > self.length - 1:
+            return False
+        elif index == 0:
+            return self.pop_first()
+        elif index == self.length - 1:
+            return self.pop()
+        else:
+            current = self.get(index)
+            next = current.next
+            before = current.prev
+            before.next = next
+            next.prev = before
 
+        self.length -= 1
+        return current
 
-
-
-
-
-
-
-    ## WRITE INSERT METHOD HERE ##
+    ## WRITE REMOVE METHOD HERE ##
     #                            #
     #                            #
     #                            #
@@ -126,28 +139,27 @@ class DoublyLinkedList:
 
 
 my_doubly_linked_list = DoublyLinkedList(1)
+my_doubly_linked_list.append(2)
 my_doubly_linked_list.append(3)
+my_doubly_linked_list.append(4)
+my_doubly_linked_list.append(5)
 
-
-print('DLL before insert():')
+print('DLL before remove():')
 my_doubly_linked_list.print_list()
 
-
-my_doubly_linked_list.insert(1,2)
-
-print('\nDLL after insert(2) in middle:')
+print('\nRemoved node:')
+print(my_doubly_linked_list.remove(2).value)
+print('DLL after remove() in middle:')
 my_doubly_linked_list.print_list()
 
-
-my_doubly_linked_list.insert(0,0)
-
-print('\nDLL after insert(0) at beginning:')
+print('\nRemoved node:')
+print(my_doubly_linked_list.remove(0).value)
+print('DLL after remove() of first node:')
 my_doubly_linked_list.print_list()
 
-
-my_doubly_linked_list.insert(4,4)
-
-print('\nDLL after insert(4) at end:')
+print('\nRemoved node:')
+print(my_doubly_linked_list.remove(2).value)
+print('DLL after remove() of last node:')
 my_doubly_linked_list.print_list()
 
 
@@ -155,26 +167,33 @@ my_doubly_linked_list.print_list()
 """
     EXPECTED OUTPUT:
     ----------------
-    DLL before insert():
-    1
-    3
-
-    DLL after insert(2) in middle:
-    1
-    2
-    3
-
-    DLL after insert(0) at beginning:
-    0
-    1
-    2
-    3
-
-    DLL after insert(4) at end:
-    0
+    DLL before remove():
     1
     2
     3
     4
+    5
+
+    Removed node:
+    3
+    DLL after remove() in middle:
+    1
+    2
+    4
+    5
+
+    Removed node:
+    1
+    DLL after remove() of first node:
+    2
+    4
+    5
+
+    Removed node:
+    5
+    DLL after remove() of last node:
+    2
+    4
 
 """
+
