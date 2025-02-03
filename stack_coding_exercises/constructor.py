86004e852d4ea7207ff8eca5e1d8327d2c7b6118
Namsraiav-Davaa <namsraijav0415@gmail.com>
2025-02-03 09:26:31 +0800

    stack push

--- stack_coding_exercises/constructor.py
+++ stack_coding_exercises/constructor.py
@@ -16,24 +16,38 @@ class Stack:
             print(temp.value)
             temp = temp.next
 
-    ### WRITE PUSH METHOD HERE ###
-    #                            #
-    #                            #
-    #                            #
-    #                            #
-    ##############################
+    def push(self, value):
+        new_node = Node(value)
+        if self.height == 0:
+            self.top = new_node
+        else:
+            new_node.next = self.top
+            self.top = new_node
+        self.height += 1
+        return True
+
+    ### WRITE POP METHOD HERE ###
+    #                           #
+    #                           #
+    #                           #
+    #                           #
+    #############################
 
 
 
 
-my_stack = Stack(2)
-
-print('Stack before push(1):')
-my_stack.print_stack()
-
+my_stack = Stack(4)
+my_stack.push(3)
+my_stack.push(2)
 my_stack.push(1)
 
-print('\nStack after push(1):')
+print('Stack before pop():')
+my_stack.print_stack()
+
+print('\nPopped node:')
+print(my_stack.pop().value)
+
+print('\nStack after pop():')
 my_stack.print_stack()
 
 
@@ -41,11 +55,18 @@ my_stack.print_stack()
 """
     EXPECTED OUTPUT:
     ----------------
-    Stack before push(1):
-    2
-
-    Stack after push(1):
+    Stack before pop():
     1
     2
+    3
+    4
+
+    Popped node:
+    1
+
+    Stack after pop():
+    2
+    3
+    4
 
 """
