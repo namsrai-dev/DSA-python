0d4545598ddd7af93ef611b5dee134f12abcaf3f
Namsraiav-Davaa <namsraijav0415@gmail.com>
2025-02-16 15:03:57 +0800

    BST insert

--- stack_coding_exercises/stack_using_dequeue.py
+++ stack_coding_exercises/stack_using_dequeue.py
@@ -10,6 +10,20 @@ class MyQueue:
         while len(self.stack2) > 0:
             self.stack1.append(self.stack2.pop())
 
+    def dequeue(self):
+        temp = None
+
+        if len(self.stack1):
+            temp = self.stack1.pop()
+
+        while len(self.stack1) > 0:
+            self.stack2.append(self.stack1.pop())
+
+        while len(self.stack2) > 0:
+            self.stack1.append(self.stack2.pop())
+        return temp
+
+
     # WRITE DEQUEUE MEHTOD HERE #
     #                           #
     #                           #
