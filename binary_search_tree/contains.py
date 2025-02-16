4066eaf61ba5500e119ffe4c63d7ec1236586597
namsrai-dev <namsrai.dev@gmail.com>
2025-02-16 22:47:45 +0800

    BST contains

---
+++ binary_search_tree/contains.py
@@ -0,0 +1,57 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+
+        temp = self.root
+        while(True):
+            if temp.value == new_node.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+    
+    def contains(self, value):
+        temp = self.root
+        while temp is not None:
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return True
+            
+        return False
+
+
+my_tree = BinarySearchTree()
+
+my_tree.insert(2)
+my_tree.insert(1)
+my_tree.insert(3)
+my_tree.insert(4)
+my_tree.insert(7)
+my_tree.insert(9)
+
+print(my_tree.contains(11))
+print(my_tree.contains(9))
+
