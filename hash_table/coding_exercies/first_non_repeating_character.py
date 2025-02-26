b24d92b9482e1bb6174cfd2fe11c6593cdd615f7
namsrai-dev <namsrai.dev@gmail.com>
2025-02-26 08:31:23 +0800

    push

---
+++ hash_table/coding_exercies/first_non_repeating_character.py
@@ -0,0 +1,27 @@
+# WRITE THE FUNCTION HERE #
+#                         #
+#                         #
+#                         #
+#                         #
+###########################
+
+def first_non_repeating_char(char):
+    ret = ""
+    return ret
+
+print( first_non_repeating_char('leetcode') )
+
+print( first_non_repeating_char('hello') )
+
+print( first_non_repeating_char('aabbcc') )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    l
+    h
+    None
+
+"""
