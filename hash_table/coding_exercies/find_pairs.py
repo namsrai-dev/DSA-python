5c7e39d421f604c5604d7bd9feffee8f082093db
namsrai-dev <namsrai.dev@gmail.com>
2025-03-06 08:50:02 +0800

    attempt 1

---
+++ hash_table/coding_exercies/find_pairs.py
@@ -0,0 +1,34 @@
+# WRITE FIND_PAIRS FUNCTION HERE #
+#                                #
+#                                #
+#                                #
+#                                #
+##################################
+
+def find_pairs(arr1, arr2, target):
+    my_dict = {}
+    for i in arr2:
+        my_dict[i] = True
+    arr = []
+    for i in arr1:
+        if target - i in my_dict:
+            arr.append((i, target - i))
+    return arr
+
+
+
+arr1 = [1, 2, 3, 4, 5]
+arr2 = [2, 4, 6, 8, 10]
+target = 7
+
+pairs = find_pairs(arr1, arr2, target)
+print (pairs)
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [(5, 2), (3, 4), (1, 6)]
+
+"""
