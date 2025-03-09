f7275ffc96f331ce0f0a4f11ebd2c17156d95c7d
namsrai-dev <namsrai.dev@gmail.com>
2025-03-09 17:19:28 +0800

    heap remove

--- heap/remove.py
+++ heap/remove.py
@@ -44,7 +44,18 @@ class MaxHeap:
                 index = max_index
             else:
                 return
-                       
+
+
+    def remove(self):
+        if len(self.heap) == 0:
+            return None
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        
+        max_value = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
+        return max_value
     # WRITE THE REMOVE METHOD HERE #
     #                              #
     #                              #
