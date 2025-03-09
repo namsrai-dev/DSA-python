afa92dbf1120bbb96da83693db15126b70df3188
namsrai-dev <namsrai.dev@gmail.com>
2025-03-09 18:09:53 +0800

    heap sink down done

--- heap/sink_down.py
+++ heap/sink_down.py
@@ -22,13 +22,24 @@ class MaxHeap:
             self._swap(current, self._parent(current))
             current = self._parent(current)
 
-    # WRITE THE _SINK_DOWN METHOD HERE #
-    #                                  #
-    #                                  #
-    #                                  #
-    #                                  #
-    ####################################
-                       
+    
+
+    def _sink_down(self, index):
+        max_index = index
+        while True:
+            left_index = self._left_child(max_index)
+            right_index = self._right_child(max_index)
+            if left_index < len(self.heap) and self.heap[left_index] > self.heap[max_index]:
+                max_index = left_index
+            if right_index < len(self.heap) and self.heap[right_index] > self.heap[max_index]:
+                max_index = right_index
+            
+            if max_index != index:
+                self._swap(index, max_index)
+                index = max_index
+            else:
+                return
+            
     def remove(self):
         if len(self.heap) == 0:
             return None
