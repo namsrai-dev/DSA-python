73ef1ec54a06fc94f0b01bec489f5e7e95d21d9f
namsrai-dev <namsrai.dev@gmail.com>
2025-04-16 09:19:00 +0800

    kth smallest node done

---
+++ basic_sorts/buble_sort.py
@@ -0,0 +1,25 @@
+## WRITE BUBBLE_SORT FUNCTION HERE ##
+#                                   #
+#                                   #
+#                                   #
+#                                   #
+##################################### 
+
+
+def bubble_sort(my_list):
+    for i in range(len(my_list) - 1, 0, -1):
+        for j in range(i-1):
+            print("j", i)
+
+
+
+print(bubble_sort([4,2,6,5,1,3]))
+
+ 
+ 
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [1, 2, 3, 4, 5, 6]
+    
+ """
