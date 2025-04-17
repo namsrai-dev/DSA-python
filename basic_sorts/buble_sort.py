8d1a950c37204881e397abba1c41e927e2a6b419
namsrai-dev <namsrai.dev@gmail.com>
2025-04-17 09:21:30 +0800

    buble sort done

--- basic_sorts/buble_sort.py
+++ basic_sorts/buble_sort.py
@@ -1,25 +1,29 @@
-## WRITE BUBBLE_SORT FUNCTION HERE ##
-#                                   #
-#                                   #
-#                                   #
-#                                   #
-##################################### 
 
+# Bubble Sort
+# Write a function called bubble_sort that sorts a list of integers in ascending order using the Bubble Sort algorithm.
+
+# The function should perform the following tasks:
+
+# Accept a parameter my_list that represents the list of integers to be sorted.
+
+# Iterate through the list from the last element to the first element. For each element i, perform the following steps:
+
+# Iterate through the list from the first element to the element at position i - 1. For each element j, perform the following steps:
+
+# Compare the element at position j with the element at position j + 1. If the element at position j is greater than the element at position j + 1, swap the two elements.
+
+# Return the sorted list.
 
 def bubble_sort(my_list):
-    for i in range(len(my_list) - 1, 0, -1):
+    for i in range(len(my_list), 0, -1):
         for j in range(i-1):
-            print("j", i)
+            if my_list[j+1] < my_list[j]:
+                temp = my_list[j+1]
+                my_list[j+1] = my_list[j]
+                my_list[j] = temp
+            print("compared two index", j, j+1)
+    return my_list
 
 
 
-print(bubble_sort([4,2,6,5,1,3]))
-
- 
- 
-"""
-    EXPECTED OUTPUT:
-    ----------------
-    [1, 2, 3, 4, 5, 6]
-    
- """
+print(bubble_sort([4,2,6,5,1,3])) 
