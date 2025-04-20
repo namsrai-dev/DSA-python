e27a1af467ad5a53068d661b66fd0452e63750da
namsrai-dev <namsrai.dev@gmail.com>
2025-04-20 14:06:04 +0800

    buble sort of linked list

--- basic_sorts/coding_challenge/bubble_sort_of_ll.py
+++ basic_sorts/coding_challenge/bubble_sort_of_ll.py
@@ -91,7 +91,20 @@ class LinkedList:
         self.length += 1
 
     def bubble_sort(self):
-        pass
+        if self.length < 2:
+            return
+        
+        sorted_until = None
+        
+        while sorted_until != self.head.next:
+            current = self.head
+            while current.next != sorted_until:
+                next_node = current.next
+                if current.value > next_node.value:
+                    current.value, next_node.value = next_node.value, current.value
+                current = current.next
+            sorted_until = current
+
 
 
 my_linked_list = LinkedList(4)
